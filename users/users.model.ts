84332520d69817154e9f38b69be67a497bdf2fbf
Luiz Cardoso <c.luizhenrique@gmail.com>
2018-08-04 23:11:53 -0300

    aula 39
    refactor de routes e inclusão de restaurant model

--- users/users.model.ts
+++ users/users.model.ts
@@ -43,29 +43,33 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-userSchema.pre('findOneAndUpdate', function(next){
-  if (!this.getUpdate().password){
-    next()
-  } else{
-    bcrypt.hash(this.getUpdate().password, environment.security.saltRounds)
-          .then(hash=>{
-            this.getUpdate().password = hash
-            next()
-          }).catch(next)
-  }
-})
+const hashPassword = (obj, next)=>{
+  bcrypt.hash(obj.password, environment.security.saltRounds)
+        .then(hash=>{
+          obj.password = hash
+          next()
+        }).catch(next)
+}
 
-userSchema.pre('save', function(next){
-  const user: User = null
-  if (!user.isModified('password')){
+const saveMiddleware = function (next){
+  const user: User = this
+  if(!user.isModified('password')){
     next()
-  } else{
-    bcrypt.hash(user.password, environment.security.saltRounds)
-          .then(hash=>{
-            user.password = hash
-            next()
-          }).catch(next)
+  }else{
+    hashPassword(user, next)
   }
-})
+}
+
+const updateMiddleware = function (next){
+  if(!this.getUpdate().password){
+    next()
+  }else{
+    hashPassword(this.getUpdate(), next)
+  }
+}
+
+userSchema.pre('save', saveMiddleware)
+userSchema.pre('findOneAndUpdate', updateMiddleware)
+userSchema.pre('update', updateMiddleware)
 
  export const User = mongoose.model<User>('User', userSchema)
